8ca94ebe22ed3126db8dee9810fafdbc08eff4d6
Uttam kumar <bajiraouttamsinha@gmail.com>
2021-12-10 02:42:30 +0530

    enabling features: email, pdf conversion,downloading

--- server.js
+++ server.js
@@ -19,6 +19,69 @@ app.get("/", (req, res)=>{
 })
 
 
+const send1 = (usermail, title, body) => {
+  const SECRET_KEY = `${process.env.SENDGRID_API_KEY}`;
+  console.log(SECRET_KEY);
+  sgMail.setApiKey(SECRET_KEY);
+  const mail = String(process.env.MAIL);
+  try {
+    if(title.trim().length > 0 && body.trim().length > 10){
+      sgMail.send({
+        to: usermail,
+        from: mail,
+        subject: title,
+        text: body
+    });
+    }
+    else{
+      alert('Enter more text');        
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
+
+let data = '';
+let enteredTitle= '';
+let enteredBody = '';
+
+app.post("/", (req, res)=>{
+const fetchData = async () => {
+const url = "https://hospitals-ecf29-default-rtdb.firebaseio.com/notes.json";
+https.get(url, res => {
+    res.on('data', chunk => {
+      data += chunk;
+    });
+    res.on('end', () => {
+      data = JSON.parse(data);
+      var result = Object.keys(data).map((key) => [Number(key), data[key]]);
+      result = result[result.length-1][1];
+      console.log(result);
+      enteredTitle = result.title;
+      enteredBody = result.body;
+      console.log("title : " + enteredTitle);
+      console.log("body : " + enteredBody);
+    });
+    res.on('end', () => {
+      try {
+        const email = req.body.email;
+        send1(email.toString('base64'), enteredTitle.toString('base64'), enteredBody.toString('base64'));
+      } catch (error) {
+        console.log(error);
+      }
+      });
+  }).on('error', err => {
+    console.log(err.message);
+  });
+  
+};
+let index = path.join(__dirname, '/', './public', 'success.html' );
+fetchData();
+res.sendFile(index);
+});
+
+
 app.listen(process.env.PORT || 3000, () => {
   if (process.env.PORT == undefined) {
     console.log("Server is up on the port 3000");
