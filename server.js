37b8c2beeaf39870e2f59d63e230012f44d60377
Uttam kumar <bajiraouttamsinha@gmail.com>
2021-12-13 20:23:40 +0530

    fixing datree checks

--- server.js
+++ server.js
@@ -52,31 +52,41 @@ const send1 = (usermail, title, body) => {
 
 }
 
-let data = '';
+let data = "";
 let enteredTitle= '';
 let enteredBody = '';
-
+let i = 0;
 app.post("/", (req, res)=>{
   const fetchData = async () => {
     const url = "https://hospitals-ecf29-default-rtdb.firebaseio.com/notes.json";
     https.get(url, res => {
         res.on('data', chunk => {
-          data += chunk;
+          data +=chunk;
         });
         res.on('end', () => {
-          data = JSON.parse(data);
-          var result = Object.keys(data).map((key) => [Number(key), data[key]]);
-          result = result[result.length-1][1];
-          console.log(result);
-          enteredTitle = result.title;
-          enteredBody = result.body;
-          console.log("title : " + enteredTitle);
-          console.log("body : " + enteredBody);
+          data = JSON.stringify(data);
+          let ans = JSON.parse(data);
+          // console.log(typeof ans + " " + ans);
+          var result = Object.keys(ans).map((key) => [Number(key), ans[key]]);
+          ans = result.toString('utf8')
+          console.log(typeof result + " " + ans);
+          // ans = ans.toString();
+          // ans = ans[Object.keys(ans)[Object.keys(ans).length - 1]];
+          // console.log(typeof ans);
+          // console.log("chunk-> " + data[data.length-1]);
+          // data = JSON.parse(data);
+          // var result = Object.keys(data).map((key) => [Number(key), data[key]]);
+          // result = result[result.length-1][1];
+          // console.log(result);
+          // enteredTitle = result.title;
+          // enteredBody = result.body;
+          // console.log("title : " + enteredTitle);
+          // console.log("body : " + enteredBody);
         });
         res.on('end', () => {
           try {
             const email = req.body.email;
-            send1(email.toString('base64'), enteredTitle.toString('base64'), enteredBody.toString('base64'));
+            // send1(email.toString('base64'), enteredTitle.toString('base64'), enteredBody.toString('base64'));
           } catch (error) {
             console.log(error);
           }
